810d96eb592420958237876b41b95dc7218b56e2
David Munoz <davidmb081117@gmail.com>
2024-11-07 17:39:42 -0500

    Se crea el proyecto con los ejemplos de los fundamentos de JavaScript

---
+++ FundamentosJS/Recursos/js/Main.js
@@ -0,0 +1,40 @@
+//document.getElementById("demo").innerHTML = "Hello JavaScript";
+//document.getElementById("demo").style.fontSize = "20px";
+
+/* Salidas de datos */
+//window.alert(5+2);
+console.log(6+2);
+
+/* Declaracion */
+let a, b, c;
+a = 5;
+b = 6;
+c = a + b;
+//window.alert(c);
+document.getElementById("demo").innerHTML = c;
+
+/* Declaracion */
+let p, q, r;
+p = 5;
+q = 7;
+const m = 8;
+r = p + q + m;
+//window.alert(r);
+document.getElementById("demo1").innerHTML = r;
+
+//Tipos de datos
+let num = 24;
+let name = "David Muñoz";
+let km = 14.5;
+let obj = {firsname:"Ana", lastname:"Perez"};
+let isColombiano = true;
+
+ let y = 17 + "POO";
+ let unir = "17" + "POO";
+ //window.alert(y);
+ //window.alert(unir)
+
+ //funcion
+ function myFunction(){
+    document.getElementById("demo2").innerHTML = "New paragraph";
+ }
